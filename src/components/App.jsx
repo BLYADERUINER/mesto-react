50ec13435f5db9e0646eead33fa4ded2de908d8c
Dmitry Alekseev <Ezzy-Way@yandex.ru>
2023-02-27 16:27:25 +0400

    added/PopupWithForm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,104 +1,96 @@
 import React from "react";
-import Footer from "./Footer.jsx";
 import Header from "./Header.jsx";
 import Main from "./Main.jsx";
-
+import Footer from "./Footer.jsx";
+import PopupWithForm from "./PopupWithForm.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <Main
-        handleEditAvatarCLick={ () => document.querySelector('.pop-up_avatar-edit').classList.add('pop-up_opened')}
-        handleEditProfileClick={ () => document.querySelector('.pop-up_profile-edit').classList.add('pop-up_opened')}
-        handleAddPlaceClick={ () => document.querySelector('.pop-up_elements').classList.add('pop-up_opened')}
+        handleEditAvatarCLick={() =>
+          document
+            .querySelector(".pop-up_edit-avatar")
+            .classList.add("pop-up_opened")
+        }
+        handleEditProfileClick={() =>
+          document
+            .querySelector(".pop-up_edit-profile")
+            .classList.add("pop-up_opened")
+        }
+        handleAddPlaceClick={() =>
+          document
+            .querySelector(".pop-up_add-card")
+            .classList.add("pop-up_opened")
+        }
       />
       <Footer />
-      <div className="pop-up pop-up_profile-edit">
-        <div className="pop-up__container">
-          <button
-            className="button pop-up__button-close"
-            type="button"
-          ></button>
-          <h2 className="pop-up__title">Редактировать профиль</h2>
-          <form
-            className="pop-up__form"
-            id="form-edit-profile"
-            name="popupFormProfile"
-            method="post"
-          >
-            <input
-              className="pop-up__input pop-up__input_name_edit"
-              id="input-profilename"
-              name="name"
-              placeholder="Иван Иванов"
-              type="text"
-              minLength="2"
-              maxLength="40"
-              required
-              value="Жак-Ив Кусто"
-            />
-            <span className="pop-up__error" id="input-profilename-error"></span>
-            <input
-              className="pop-up__input pop-up__input_status_edit"
-              id="input-profilestatus"
-              name="about"
-              placeholder="Расхититель гробниц"
-              type="text"
-              minLength="2"
-              maxLength="400"
-              required
-              value="Исследователь океана"
-            />
-            <span
-              className="pop-up__error"
-              id="input-profilestatus-error"
-            ></span>
-            <button className="button pop-up__button-save" type="submit">
-              Сохранить
-            </button>
-          </form>
-        </div>
-      </div>
-      <div className="pop-up pop-up_elements">
-        <div className="pop-up__container">
-          <button
-            className="button pop-up__button-close"
-            type="button"
-          ></button>
-          <h2 className="pop-up__title">Новое место</h2>
-          <form
-            className="pop-up__form"
-            id="form-add-card"
-            name="popupFormCard"
-            method="post"
-          >
-            <input
-              className="pop-up__input pop-up__input_name_card"
-              id="input-namecard"
-              name="name"
-              placeholder="Название"
-              type="text"
-              minLength="2"
-              maxLength="30"
-              required
-            />
-            <span className="pop-up__error" id="input-namecard-error"></span>
-            <input
-              className="pop-up__input pop-up__input_link_image"
-              id="input-urlcard"
-              name="link"
-              placeholder="Ссылка на картинку"
-              type="url"
-              required
-            />
-            <span className="pop-up__error" id="input-urlcard-error"></span>
-            <button className="button pop-up__button-save" type="submit">
-              Создать
-            </button>
-          </form>
-        </div>
-      </div>
+        {/* Popup edit profile */}
+      <PopupWithForm popupName="edit-profile" popupTitle="Редактировать профиль" popupTextButton="Сохранить">
+        <input
+          className="pop-up__input pop-up__input_edit-userName"
+          name="userName"
+          id="input-userName"
+          placeholder="Иван Иванов"
+          type="text"
+          minLength="2"
+          maxLength="40"
+          value="Жак-Ив Кусто"
+          required
+        />
+        <span className="pop-up__error" id="input-userName-error"></span>
+        <input
+          className="pop-up__input pop-up__input_edit-userAbout"
+          name="userAbout"
+          id="input-userAbout"
+          placeholder="Расхититель гробниц"
+          type="text"
+          minLength="2"
+          maxLength="400"
+          value="Исследователь океана"
+          required
+        />
+        <span className="pop-up__error" id="input-userAbout-error"></span>
+      </PopupWithForm>
+        {/* Popup Add card */}
+      <PopupWithForm popupName="add-card" popupTitle="Новое место" popupTextButton="Создать">
+        <input
+          className="pop-up__input pop-up__input_card-name"
+          name="cardName"
+          id="input-cardName"
+          placeholder="Название"
+          type="text"
+          minLength="2"
+          maxLength="30"
+          required
+        />
+        <span className="pop-up__error" id="input-cardName-error"></span>
+        <input
+          className="pop-up__input pop-up__input_card-link"
+          name="cardLink"
+          id="input-cardLink"
+          placeholder="Ссылка на картинку"
+          type="url"
+          required
+        />
+        <span className="pop-up__error" id="input-cardLink-error"></span>
+      </PopupWithForm>
+        {/* Popup edit avatar */}
+      <PopupWithForm popupName="edit-avatar" popupTitle="Обновить аватар" popupTextButton="Сохранить">
+        <input
+          className="pop-up__input pop-up__input_avatar-link"
+          name="avatarLink"
+          id="input-avatarLink"
+          placeholder="Ссылка на картинку"
+          type="url"
+          required
+        />
+        <span className="pop-up__error" id="input-avatarLink-error"></span>
+      </PopupWithForm>
+        {/* Popup delete card  */}
+      <PopupWithForm popupName="delete-card" popupTitle="Вы уверены ?" popupTextButton="Да" />
+
       <div className="pop-up pop-up_card-image">
         <figure className="pop-up__figure">
           <img className="pop-up__image" />
@@ -109,46 +101,7 @@ function App() {
           ></button>
         </figure>
       </div>
-      <div className="pop-up pop-up_card-delete">
-        <div className="pop-up__container">
-          <button
-            className="button pop-up__button-close"
-            type="button"
-          ></button>
-          <h2 className="pop-up__title">Вы уверены ?</h2>
-          <button className="button pop-up__button-save" type="button">
-            Да
-          </button>
-        </div>
-      </div>
-      <div className="pop-up pop-up_avatar-edit">
-        <div className="pop-up__container">
-          <button
-            className="button pop-up__button-close"
-            type="button"
-          ></button>
-          <h2 className="pop-up__title">Обновить аватар</h2>
-          <form
-            className="pop-up__form"
-            id="form-edit-avatar"
-            name="popupFormAvatar"
-            method="post"
-          >
-            <input
-              className="pop-up__input pop-up__input_link_avatar"
-              id="input-urlAvatar"
-              name="link"
-              placeholder="Ссылка на картинку"
-              type="url"
-              required
-            />
-            <span className="pop-up__error" id="input-urlAvatar-error"></span>
-            <button className="button pop-up__button-save" type="submit">
-              Сохранить
-            </button>
-          </form>
-        </div>
-      </div>
+
       <template id="card">
         <article className="element">
           <img className="element__image" />
---
+++ src/components/PopupWithForm.jsx
@@ -0,0 +1,18 @@
+function PopupWithForm({ popupName, popupTitle, popupTextButton, children }) {
+  return (
+    <div className={`pop-up pop-up_${popupName}`}>
+      <div className="pop-up__container">
+        <button className="button pop-up__button-close" type="button"></button>
+        <h2 className="pop-up__title">{`${popupTitle}`}</h2>
+        <form className="pop-up__form" name={`${popupName}`} method="post">
+          {children}
+          <button className="button pop-up__button-save" type="submit">
+            {popupTextButton}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default PopupWithForm;
